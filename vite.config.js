2049ee6a1bac3a36dd987018cb1e6aa492f80f01
Idowu Adeniyi <adeniyi.idowu500@gmail.com>
2025-04-17 22:06:27 -0400

    updated react built

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 5000,
   },
   preview: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 5000,
   },
 });
